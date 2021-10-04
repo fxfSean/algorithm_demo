759cef266a82d2953cda9e5fff50780b868f7059
fuxiaofang <fuxiaofang93@gmail.com>
2021-10-04 17:14:46 +0800

    添加滑动窗口伪代码

--- app/src/test/java/com/fxf/leetcode/slidingwindow/MinestSubStr.kt
+++ app/src/test/java/com/fxf/leetcode/slidingwindow/MinestSubStr.kt
@@ -36,6 +36,43 @@ public class MinestSubStr {
      *  return res
      * }
      */
+    fun minWin(s: String, t: String) : String {
+        var len = Int.MAX_VALUE
+        var start = 0
+        var left = 0
+        var right = 0
+        var valid = 0
+        var need = HashMap<Char, Int>()
+        var window = HashMap<Char, Int>()
+        val tArray = t.toCharArray()
+        for (item in tArray) {
+            need[item] = (need[item] ?: 0).plus(1)
+        }
+        val sArray = s.toCharArray()
+        while (right < s.length) {
+            val r = sArray[right]
+            right++
+            if (need.containsKey(r)) {
+                window[r] = (window[r] ?:0).plus(1)
+                if (window[r] == need[r]) {
+                    valid++
+                }
+            }
+            while (valid == need.size) {
+                if (right - left < len) {
+                    start = left
+                    len = right - left
+                }
+                val c = sArray[left]
+                left++
+                if (need.containsKey(c)) {
+                    valid--
+                }
+                window[c] = window[c]!!.minus(1)
+            }
+        }
+        return if (len == Int.MAX_VALUE) "" else s.substring(start, start+len)
+    }
     fun minWindow(s: String, t: String): String {
         var left=0
         var right = 0
